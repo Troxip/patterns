579d8cfdf2a270f6b35b0ca183357593b0a6267a
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-20 22:59:02 +0100

    Accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import Accordion from "./components/Accordian/Accordian";
+import AccordionItem from "./components/Accordian/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            className="accordion-item"
+            title="We got 20 years of experience"
+          >
+            <article>
+              <p>You can't go wrong with us</p>
+              <p>
+                We are in the business of planning high We are in the business
+                of planning high
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            className="accordion-item"
+            title="We are working with local guides"
+          >
+            <article>
+              <p>We are not doing this along from out office</p>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto
+                tenetur ratione iste voluptatum nisi quis voluptate inventore
+                vitae consequatur, omnis culpa asperiores officiis sint aliquam,
+                est explicabo, itaque odio id.
+              </p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
