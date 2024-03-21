5579570576c40e6b0e144f56c4e93e8bf3fca732
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-21 18:00:10 +0100

    Shorter code

--- src/components/Accordian/AccordionItem.jsx
+++ src/components/Accordian/AccordionItem.jsx
@@ -1,21 +1,13 @@
 import { useAccordionContext } from "./Accordian";
 
 export default function AccordionItem({ id, className, title, children }) {
-  const { openItemId, openItem, closeItem } = useAccordionContext();
+  const { openItemId, toggleItem } = useAccordionContext();
 
   const isOpen = openItemId === id;
 
-  function handleClick() {
-    if (isOpen) {
-      closeItem();
-    } else {
-      openItem(id);
-    }
-  }
-
   return (
     <li className={className}>
-      <h3 onClick={handleClick}>{title}</h3>
+      <h3 onClick={() => toggleItem(id)}>{title}</h3>
       {openItemId === id && (
         <div
           className={
